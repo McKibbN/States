6cd25f816f1054cd0ce322a6930e80b1377b9d18
Durban-Designer <royce.birnbaum@gmail.com>
2017-08-24 14:39:13 -0700

    Finished that Shit

--- server.js
+++ server.js
@@ -3,7 +3,19 @@ var lessMiddleware = require('less-middleware');
 var app = express();
 var router = express.Router();
 var path = __dirname + '/views/';
-var states = require("./states.json");
+var states = [
+{id:"CA", state_name:"California", population:38.8},
+{id:"TX", state_name:"Texas", population:27},
+{id:"FL", state_name:"Florida", population:19.9},
+{id:"NY", state_name:"New York", population:19.8},
+{id:"IL", state_name:"Illinois", population:12.9},
+{id:"PA", state_name:"Pennsylvania", population:12.8},
+{id:"OH", state_name:"Ohio", population:11.6},
+{id:"GA", state_name:"Georgia", population:10.1},
+{id:"NC", state_name:"North Carolina", population:10},
+{id:"MI", state_name:"Michigan", population:9.9},
+];
+console.log(states);
 
 app.use(lessMiddleware(path));
 app.use(express.static(path));
@@ -20,13 +32,14 @@ app.get('/states', function(req,res) {
 
 app.get('/states/:id', function(req,res) {
   let stateId = req.params['id'];
-  let matches = states.filter(s => s._id == stateId);
+  let matches = states.filter(s => s.id == stateId);
+  console.log(matches);
   if (!matches.length) {
     res.status(404).send(`No matching state for id ${stateId} there are ${state.length}`);
   } else {
     res.locals.state = matches[0];
+    res.send(`${matches[0].population} Million.`);
   }
-  res.send("${states.population} Million.");
 });
 
 app.listen(3000,function(){
